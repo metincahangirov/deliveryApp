5711bd75c76ddf50bbc9ddc9b9457f3795ac830b
vuqarcahangirli1 <vuqarcahangirli1@gmail.com>
2026-04-09 02:30:03 +0400

    Delete src directory

--- src/main/java/com/example/notificationservice_ms/notification/NotificationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificationservice_ms.notification;
-
-public class NotificationNotFoundException extends RuntimeException {
-    public NotificationNotFoundException(String message) {
-        super(message);
-    }
-}
